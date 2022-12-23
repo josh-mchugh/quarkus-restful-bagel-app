041540f2810208360f1b5e2628e0dce71540ced9
Josh McHugh <joshua.mchugh01@gmail.com>
2022-12-22 23:45:44 -0500

    Added MenuItemResource

--- src/main/java/com/communitybagelco/menu/MenuItemResource.java
+++ src/main/java/com/communitybagelco/menu/MenuItemResource.java
@@ -1,5 +1,24 @@
 package com.communitybagelco.menu;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import com.communitybagelco.menu.repository.MenuItemRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Path("/menu")
+@ApplicationScoped
+@RequiredArgsConstructor
 public class MenuItemResource {
     
+    private final MenuItemRepository repository;
+
+    @GET
+    public Response getMenu() {
+
+        return Response.ok(repository.findAll()).build();
+    }
 }
--- src/test/java/com/communitybagelco/menu/MenuItemResourceTest.java
+++ src/test/java/com/communitybagelco/menu/MenuItemResourceTest.java
@@ -1,5 +1,45 @@
 package com.communitybagelco.menu;
 
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
 public class MenuItemResourceTest {
     
+    @Test
+    public void whenGetMenuThenExpectOk() {
+
+        given()
+            .when()
+                .get("/menu")
+            .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    public void whenGetMenuThenExpectBody() {
+
+        given()
+            .when()
+                .get("/menu")
+            .then()
+                .body("[0].productId", is(1))
+                .body("[0].name", is("Plain"))
+                .body("[0].price", is(1.75F))
+                .body("[1].productId", is(2))
+                .body("[1].name", is("Everything"))
+                .body("[1].price", is(1.75F))
+                .body("[2].productId", is(3))
+                .body("[2].name", is("Poppy"))
+                .body("[2].price", is(1.75F))
+                .body("[3].productId", is(4))
+                .body("[3].name", is("Onion"))
+                .body("[3].price", is(1.75F));
+    }
 }
