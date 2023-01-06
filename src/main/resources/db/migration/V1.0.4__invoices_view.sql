598eae25f50794b41cda8c4a10cdff20b91798cb
Josh McHugh <joshua.mchugh01@gmail.com>
2023-01-05 21:29:33 -0500

    Added tests for InvoiceRepository. Updated invoice_view to return Order with have no items

--- src/main/resources/db/migration/V1.0.4__invoices_view.sql
+++ src/main/resources/db/migration/V1.0.4__invoices_view.sql
@@ -4,6 +4,6 @@ SELECT
     o.timestamp AS timestamp,
     SUM(p.price * od.quantity) AS total
 FROM orders o
-INNER JOIN order_details od ON o.order_id = od.order_id
-INNER JOIN products p ON od.product_id = p.product_id
+LEFT JOIN order_details od ON o.order_id = od.order_id
+LEFT JOIN products p ON od.product_id = p.product_id
 GROUP BY o.order_id
--- src/test/java/com/communitybagelco/invoice/entity/InvoiceRepositoryTest.java
+++ src/test/java/com/communitybagelco/invoice/entity/InvoiceRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.communitybagelco.invoice.entity;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import javax.inject.Inject;
@@ -11,6 +13,10 @@ import com.communitybagelco.invoice.entity.model.ImmutableInvoiceRequest;
 import com.communitybagelco.invoice.entity.model.InvoiceRequest;
 import com.communitybagelco.invoice.model.Invoice;
 import com.communitybagelco.order.entity.OrderRepository;
+import com.communitybagelco.order.service.model.ImmutableItem;
+import com.communitybagelco.order.service.model.ImmutableOrderRequest;
+import com.communitybagelco.order.service.model.OrderRequest;
+import com.communitybagelco.order.service.model.OrderResponse;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -33,5 +39,126 @@ public class InvoiceRepositoryTest {
         Optional<Invoice> result = repository.findByOrderId(request);
 
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void whenOrderExistsByIdThenExpectOrderId() {
+
+        Integer orderId = createOrderId();
+
+        InvoiceRequest request = ImmutableInvoiceRequest.builder()
+            .orderId(orderId)
+            .build();
+
+        Optional<Invoice> result = repository.findByOrderId(request);
+
+        Assertions.assertEquals(orderId, result.get().orderId());
+    }
+
+    @Test
+    public void whenOrderExistsThenExpectTimestampNotNull() {
+
+        Invoice invoice = createInvoice();
+
+        Assertions.assertNotNull(invoice.timestamp());
+    }
+
+    @Test
+    public void whenOrderExistsThenExpectTotal() {
+
+        Invoice invoice = createInvoice();
+
+        Assertions.assertEquals(new BigDecimal("5.25"), invoice.total());
+    }
+
+    @Test
+    public void whenOrderExistsThenExpectItemsNotNull() {
+
+        Invoice invoice = createInvoice();
+
+        Assertions.assertNotNull(invoice.items());
+    }
+
+    @Test
+    public void whenOrderDoesNotHaveItemsThenExpectEmptyItems() {
+
+        OrderRequest orderRequest = ImmutableOrderRequest.builder().build();
+        OrderResponse orderResponse = orderRepository.create(orderRequest);
+
+        InvoiceRequest request = ImmutableInvoiceRequest.builder()
+            .orderId(orderResponse.orderId())
+            .build();
+
+        Optional<Invoice> invoice = repository.findByOrderId(request);
+
+        Assertions.assertEquals(0, invoice.get().items().size());
+    }
+
+    @Test
+    public void whenOrderExistsThenExpectItemsSize() {
+
+        Invoice invoice = createInvoice();
+
+        Assertions.assertEquals(2, invoice.items().size());
+    }
+
+    @Test
+    public void whenOrderExiststhenExpectItems() {
+
+        Invoice invoice = createInvoice();
+
+        Assertions.assertEquals(expectedItems(), invoice.items());
+    }
+
+    private List<Invoice.Item> expectedItems() {
+
+        Invoice.Item plainBagel = com.communitybagelco.invoice.model.ImmutableItem.builder()
+            .productId(1)
+            .productName("Plain")
+            .quantity(1)
+            .total(new BigDecimal("1.75"))
+            .build();
+
+        Invoice.Item everythingBagel = com.communitybagelco.invoice.model.ImmutableItem.builder()
+            .productId(2)
+            .productName("Everything")
+            .quantity(2)
+            .total(new BigDecimal("3.5"))
+            .build();
+
+        return List.of(plainBagel, everythingBagel);
+    }
+
+    private Invoice createInvoice() {
+
+        InvoiceRequest request = ImmutableInvoiceRequest.builder()
+            .orderId(createOrderId())
+            .build();
+
+        return repository.findByOrderId(request).get();
+    }
+
+    private Integer createOrderId() {
+
+        OrderRequest orderRequest = createOrderRequest();
+        
+        return orderRepository.create(orderRequest).orderId();
+    }
+
+    private OrderRequest createOrderRequest() {
+
+        OrderRequest.Item plainBagelItem = ImmutableItem.builder()
+            .productId(1)
+            .quantity(1)
+            .build();
+
+        OrderRequest.Item everythingBagelItem = ImmutableItem.builder()
+            .productId(2)
+            .quantity(2)
+            .build();
+
+        return ImmutableOrderRequest.builder()
+            .addItems(plainBagelItem, everythingBagelItem)
+            .build();
     }
 }
