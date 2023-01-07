f9a720e00eaf7328e9c20d3b19570d49fd4122d1
Josh McHugh <joshua.mchugh01@gmail.com>
2023-01-06 22:21:30 -0500

    Added tests for InvoiceRequest

--- src/test/java/com/communitybagelco/invoice/entity/model/InvoiceRequsetTest.java
+++ src/test/java/com/communitybagelco/invoice/entity/model/InvoiceRequsetTest.java
@@ -1,5 +1,104 @@
 package com.communitybagelco.invoice.entity.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
 public class InvoiceRequsetTest {
+
+    @Test
+    public void whenRequestHasOrderIdThenExpectOrderId() {
+
+        InvoiceRequest request = ImmutableInvoiceRequest.builder()
+            .orderId(1)
+            .build();
+
+        Assertions.assertEquals(1, request.orderId());
+    }
+    
+    @Test
+    public void whenRequestHasNullOrderIdThenExpectException() {
+
+        ImmutableInvoiceRequest.Builder builder = ImmutableInvoiceRequest.builder();
+
+        Assertions.assertThrows(IllegalStateException.class, () -> builder.build());
+    }
+
+    @Test
+    public void whenRequestToStringThenExpectToString() {
+
+        String expected = "InvoiceRequest{orderId=1}";
+
+        InvoiceRequest request = createRequest();
+
+        Assertions.assertEquals(expected, request.toString());
+    }
+
+    @Test
+    public void whenRequestHashCodeThenExpectValue() {
+
+        InvoiceRequest request = createRequest();
+
+        Assertions.assertEquals(177574, request.hashCode());
+    }
+
+    @Test
+    public void whenRequestEqualsIsSameInstanceThenExpectTrue() {
+
+        InvoiceRequest request1 = createRequest();
+        InvoiceRequest request2 = request1;
+
+        Assertions.assertTrue(request1.equals(request2));
+    }
+
+    @Test
+    public void whenRequestEqualsIsDifferentTypeThenExpectFalse() {
+
+        InvoiceRequest request = createRequest();
+        Object object = new Object();
+
+        Assertions.assertFalse(request.equals(object));
+    }
+
+    @Test
+    public void whenRequestEqualsIsTrueThenExpectTrue() {
+
+        InvoiceRequest request1 = createRequest(1);
+        InvoiceRequest request2 = createRequest(1);
+
+        Assertions.assertTrue(request1.equals(request2));
+    }
+
+    @Test
+    public void whenRequestEqualsIsNotEqualThenExpectFalse() {
+
+        InvoiceRequest request1 = createRequest(1);
+        InvoiceRequest request2 = createRequest(2);
+
+        Assertions.assertFalse(request1.equals(request2));
+    }
+
+    @Test
+    public void invoiceRequestEqualsContract() {
+
+        EqualsVerifier.simple().forClass(ImmutableInvoiceRequest.class)
+            .withNonnullFields("orderId")
+            .withResetCaches()
+            .verify();
+    }
+
+    private InvoiceRequest createRequest() {
+
+        return createRequest(1);
+    }
+
+    private InvoiceRequest createRequest(Integer orderId) {
+
+        return ImmutableInvoiceRequest.builder()
+            .orderId(orderId)
+            .build();
+    }
+
     
 }
